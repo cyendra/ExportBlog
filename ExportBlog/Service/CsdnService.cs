b0347f6f27c49e6ca8f12088ab074ded7be22798
cyendra <g1483335912@gmail.com>
2014-07-28 20:26:47 +0800

    web

---
+++ ExportBlog/Service/CsdnService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ExportBlog.Service
+{
+    internal class CsdnService : IFeedService
+    {
+        string url = null;
+
+        Regex reg_title = new Regex(@"");
+        Regex reg_con = new Regex(@"");
+
+        WebUtility web = null;
+
+        IList<FeedEntity> IFeedService.GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IFeedService.GetContent(ref FeedEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        FeedEntity IFeedService.GetEntity(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
