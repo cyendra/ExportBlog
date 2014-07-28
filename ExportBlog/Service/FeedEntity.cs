adc111c3b622eb48576570e1713461f39a001656
cyendra <g1483335912@gmail.com>
2014-07-28 10:36:23 +0800

    feed

---
+++ ExportBlog/Service/FeedEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportBlog.Service
+{
+    public class FeedEntity
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsDown { get; set; }
+        public FeedEntity()
+        {
+            Content = string.Empty;
+            IsDown = true;
+        }
+        public void Dispose()
+        {
+            Url = null;
+            Title = null;
+            Content = null;
+        }
+    }
+    public enum Source
+    {
+        //[Description("CSDN")]
+        csdn
+    }
+}
---
+++ ExportBlog/Service/IFeedService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportBlog.Service
+{
+    internal interface IFeedService
+    {
+        /// <summary>
+        /// 获取文章列表
+        /// </summary>
+        /// <returns></returns>
+        IList<FeedEntity> GetList();
+
+        /// <summary>
+        /// 获取文章内容
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        bool GetContent(ref FeedEntity entity);
+
+        /// <summary>
+        /// 通过文章url获取内容
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        FeedEntity GetEntity(string url);
+    }
+}
