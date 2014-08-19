5af4478d4e96028ad2560b0d61ee8c535ed5f4e0
cyendra <g1483335912@gmail.com>
2014-08-19 20:25:22 +0800

    upd

--- ExportBlog/Service/CsdnService.cs
+++ ExportBlog/Service/CsdnService.cs
@@ -10,22 +10,22 @@ namespace ExportBlog.Service
     internal class CsdnService : IFeedService
     {
         string url = null;
-
-        Regex reg_title = new Regex(@"");
-        Regex reg_title2 = new Regex(@"");
-        Regex reg_con = new Regex(@"");
+        Regex reg_title = new Regex(@"<span class=""link_title""><a href=""(.+?)"">([^<]+?)</a></span>", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        Regex reg_con = new Regex(@"<div id=""article_content"" class=""article_content"">([\s\S]+)</div>\s*<div class=""share_buttons"" id=""sharePanel"">", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
         WebUtility web = null;
 
         public CsdnService(string un)
         {
-            url = "";
+            url = "http://blog.csdn.com/" + un + "/article/list/{0}?viewmode=contents";
+
             web = new WebUtility();
         }
-        
-        IList<FeedEntity> GetList()
+
+        public IList<FeedEntity> GetList()
         {
             var list = new List<FeedEntity>();
+
             int p = 0;
             for (int i = 1; i < 1000; i++)
             {
@@ -34,17 +34,22 @@ namespace ExportBlog.Service
                 string html = web.Get();
                 if (p == 0)
                 {
-                    var mp = Regex.Match(html, @"");
+                    var mp = Regex.Match(html, @"共(\d+)页");
                     if (mp.Success) p = App.ToInt(mp.Groups[1].Value);
                     else p = 1;
                 }
-                var mats = reg_title.Match(html);
+                var mats = reg_title.Matches(html);
                 if (mats.Count == 0) break;
                 foreach (Match mat in mats)
                 {
-                   
+                    var fd = new FeedEntity();
+                    fd.Url = "http://blog.csdn.com" + mat.Groups[1].Value;
+                    fd.Title = mat.Groups[2].Value.Trim();
+
+                    list.Add(fd);
                 }
             }
+            return list;
         }
 
         public bool GetContent(ref FeedEntity entity)
@@ -62,12 +67,13 @@ namespace ExportBlog.Service
         public FeedEntity GetEntity(string url)
         {
             var entity = new FeedEntity();
+
             web.URL = url;
             string html = web.Get();
-            Match mat = reg_title2.Match(html);
+            Match mat = reg_title.Match(html);
             if (mat.Success)
             {
-                entity.Title = mat.Groups[1].Value.Trim();
+                entity.Title = mat.Groups[2].Value.Trim();
             }
             mat = reg_con.Match(html);
             if (mat.Success)
---
+++ ExportBlog/Service/FeedService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportBlog.Service
+{
+    /// <summary>
+    /// 资源抓取服务
+    /// </summary>
+    public class FeedService
+    {
+        IFeedService service = null;
+        Exception excep = null;
+
+        public FeedService(Source src, string user)
+        {
+            user = user.ToLower();
+            try
+            {
+                switch (src)
+                {
+                    case Source.csdn:
+                        service = new CsdnService(user);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                excep = ex;
+            }
+        }
+        private IList<FeedEntity> _list = null;
+
+        public IList<FeedEntity> GetList()
+        {
+            if (_list == null)
+            {
+                if (excep != null)
+                {
+                    _list = new List<FeedEntity>();
+                }
+                else
+                {
+                    _list = service.GetList();
+                }
+            }
+            return _list;
+        }
+        public bool GetContent(ref FeedEntity entity)
+        {
+            if (entity.Content != string.Empty) return true;
+
+            System.Threading.Thread.Sleep(2000);
+
+            return service.GetContent(ref entity);
+        }
+
+        public FeedEntity GetEntity(string url)
+        {
+            if (_list == null)
+            {
+                _list = new List<FeedEntity>();
+            }
+            foreach (var m in _list)
+            {
+                if (m.Url == url)
+                    return m;
+            }
+            var entity = service.GetEntity(url);
+            entity.Url = url;
+            _list.Add(entity);
+            return entity;
+        }
+    }
+}
